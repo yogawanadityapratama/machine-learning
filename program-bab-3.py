22d53fde1efdcd85c4ed15b800abf534a431f750
Yogawan Aditya Pratama Toekidjo <123430193+yogawanadityapratama@users.noreply.github.com>
2024-02-26 18:15:24 +0700

    mengubah berberapa comment pada program

--- program-bab-3.py
+++ program-bab-3.py
@@ -15,7 +15,7 @@ print(dataku.head())
 print("Ukuran:", dataku.shape)
 print("Fitur:", dataku.columns)
 
-# Mengubah data kategorikal menjadi numerik menggunakan LabelEncoder
+# mengubah data kategorikal menjadi numerik menggunakan LabelEncoder
 le = LabelEncoder()
 dataku['cuaca'] = le.fit_transform(dataku['cuaca'])
 dataku['suhu'] = le.fit_transform(dataku['suhu'])
@@ -23,17 +23,17 @@ dataku['kelembaban'] = le.fit_transform(dataku['kelembaban'])
 dataku['angin'] = le.fit_transform(dataku['angin'])
 dataku['main'] = le.fit_transform(dataku['main'])
 
-# Memisahkan fitur dan target
+# memisahkan fitur dan target
 x = dataku[dataku.columns[:-1]]
 y = dataku['main']
 
-# Membagi data menjadi set pelatihan dan pengujian
+# membagi data menjadi set pelatihan dan pengujian
 tes_sizes = [0.1, 0.3, 0.5]
 for t in tes_sizes:
     print(f"ukuran data tes : {t}")
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=t, random_state=42)
 
-    # Melakukan percobaan dengan nilai k yang berbeda
+    # melakukan percobaan dengan nilai k yang berbeda
     k_values = [3, 5, 7, 9]
     for k in k_values:
         knn = KNeighborsClassifier(n_neighbors=k)
